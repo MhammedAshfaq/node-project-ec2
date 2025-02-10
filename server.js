0870e194ebc2a0737c46ce5ddbab11bd4f3a2105
Muhammed  Ashfaq <ashfaqmohammedcv@gmail.com>
2025-02-10 23:40:20 +0530

    added  new api

--- server.js
+++ server.js
@@ -8,6 +8,34 @@ app.get("/api/get", (req, res) => {
   res.send({ message: "Nodejs Server" });
 });
 
+app.get("/api/user", (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Successfully",
+      data: [
+        {
+          id: 1,
+          name: "Ashfaq",
+          email: "example@gmail.com",
+        },
+        {
+          id: 2,
+          name: "Haani",
+          email: "example1@gmail.com",
+        },
+        {
+          id: 3,
+          name: "Jomon",
+          email: "example3@gmail.com",
+        },
+      ],
+    });
+  } catch (error) {
+    console.log("Error", error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server run on http://localhost:${PORT}`);
 });
